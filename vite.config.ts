3043c4cffd6022e57b385c9a27e67124b1dac7c9
paolotsname <paulo.nery97@gmail.com>
2025-06-24 14:20:29 -0300

    vite.config.js fixed

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
+    base: '/pf2-stats-web-app/',
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
 });
